10eb67cd08b05dfd6ea8379c5f59da1c55063494
janardhan-py <janardhann1998@gmail.com>
2021-02-16 12:35:42 +0530

    editing procfile

--- Examination/settings.py
+++ Examination/settings.py
@@ -146,7 +146,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:8000"
+    "http://127.0.0.1:8000",
+    "https://online-test3.herokuapp.com"
 ]
 
 CORS_ALLOWED_ORIGIN_REGEXES = [
