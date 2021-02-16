f369fa7fd382b49cb25b0189fb6fb7d80558f4ac
janardhan-py <janardhann1998@gmail.com>
2021-02-16 13:01:53 +0530

    adding Django_heroku

--- Examination/settings.py
+++ Examination/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 
+import django_heroku
+
+#adding django heroku
+django_heroku.settings(locals())
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
